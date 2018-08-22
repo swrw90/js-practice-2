fdf2d89cfb05aebc983e50b034c88150be901050
Seth Watson <seth.watson1@gmail.com>
2018-08-22 17:09:19 -0600

    added drag functions

--- dragndrop/js/main.js
+++ dragndrop/js/main.js
@@ -5,11 +5,37 @@ const emptyBoxes = document.querySelectorAll('.empty');
 fill.addEventListener('dragstart', dragStart);
 fill.addEventListener('dragend', dragEnd);
 
+//Loop through emptyBoxes and call drag events
+for (const empty of emptyBoxes) {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
+
 //Drag Functions
 function dragStart() {
-    console.log('start');
+    this.className += ' hold';
+    setTimeout(() => (this.className = 'invisible'), 0);
 }
 
 function dragEnd() {
-    console.log('end');
+    this.className = 'fill';
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+    this.className += 'hovered';
+    
+}
+function dragLeave() {
+    this.className = 'empty';
+}
+function dragDrop() {
+    this.className = 'empty';
+    this.append(fill);
 }
